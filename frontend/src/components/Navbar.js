f2fc21083032e5f69068265d7db26f0310f049e0
gregoryadi <gregoryadi@berkeley.edu>
2024-12-02 08:54:38 -0500

    added logo photo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../photos/logo.png'; 
 
 const Navbar = () => {
     return (
-        <nav className="bg-gray-800 text-white p-4">
-            <div className="container mx-auto flex justify-between">
-                <h1 className="text-lg font-bold">ReChord</h1>
-                <ul className="flex space-x-4">
-                    <li>
-                        <Link to="/" className="hover:text-blue-300">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/news" className="hover:text-blue-300">News</Link>
-                    </li>
-                    <li>
-                        <Link to="/reviews" className="hover:text-blue-300">Reviews</Link>
-                    </li>
-                    <li>
-                        <Link to="/social" className="hover:text-blue-300">Social</Link>
-                    </li>
-                </ul>
+        <nav className="bg-gray-900 text-white p-4">
+            <div className="container mx-auto flex items-center justify-between">
+                {/* Left Section: Logo */}
+                <Link to="/" className="flex items-center space-x-4">
+                    {/* Larger logo */}
+                    <img src={logo} alt="ReChord Logo" className="w-56 h-auto" />
+                    {/*<div>
+                        <h1 className="text-2xl font-bold">
+                            <span className="text-indigo-400">Chord</span>
+                        </h1>
+                        <p className="text-sm text-gray-400">Share your sound</p>
+                    </div>*/}
+                </Link>
+
+                {/* Right Section: Navigation Links */}
+                <div className="flex items-center space-x-8">
+                    <Link
+                        to="/reviews"
+                        className="text-sm font-medium hover:underline hover:text-indigo-400"
+                    >
+                        Reviews
+                    </Link>
+                    <Link
+                        to="/news"
+                        className="text-sm font-medium underline font-bold"
+                    >
+                        News
+                    </Link>
+                    <Link
+                        to="/social"
+                        className="text-sm font-medium hover:underline hover:text-indigo-400"
+                    >
+                        Social
+                    </Link>
+                </div>
             </div>
         </nav>
     );
